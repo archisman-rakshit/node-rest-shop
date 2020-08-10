48c006728c2ba2013daaaa90784bd4d3898f1783
Archisman <archisman1997@gmail.com>
2020-08-11 02:28:18 +0530

    Added a mongoose connection and changed endpoints for products.js

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,45 +1,104 @@
 const express = require('express');
 const router = express.Router();
-
+const Product = require('../models/products');
+const mongoose = require('mongoose');
 
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Handling GET to /products'
-    });
+    Product.find()
+        .exec()
+        .then(docs =>{
+            console.log(docs);
+            if (docs.length > 0){
+                res.status(200).json(docs);
+            }
+            else {
+                res.status(404).json({
+                    message: 'No content found'
+                });
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 router.post('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Handling POST to /products'
+    const product = new Product({
+        _id: new mongoose.Types.ObjectId,
+        name: req.body.name,
+        price: req.body.price
+    });
+    product.save().
+    then(result =>{
+        console.log(result);
+        res.status(201).json({
+            createdProduct: product
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        })
     });
 });
 
 router.get('/:productId', (req, res, next) =>{
    const id = req.params.productId;
-   if (id === 'special') {
-       res.status(200).json({
-           message: 'You discovered a special id',
-           id: id
+   Product.findById(id)
+       .exec()
+       .then( doc => {
+           console.log(doc);
+           if(doc){
+               res.status(200).json(doc);
+           }
+           else{
+               res.status(404).json({error:"No valid data found"});
+           }
+       })
+       .catch(err => {
+           res.status(500).json({
+               error: err
+           });
+           console.log(err);
        });
-   }
-   else {
-       res.status(200).json({
-           message: 'You discovered an id',
-           id: id
-       });
-   }
 });
 
 router.patch('/:productId', (req, res, next) =>{
-    res.status(200).json({
-       message: 'Updated product'
-    });
+    const id = req.params.productId;
+    const updateOps = {};
+    for (const ops of req.body){
+        updateOps[ops.propName] = ops.value;
+    }
+    Product.update({_id: id}, { $set: updateOps })
+        .exec()
+        .then(result => {
+            console.log(result);
+            res.status(200).json(result);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 router.delete('/:productId', (req, res, next) =>{
-    res.status(200).json({
-        message: 'Deleted product'
-    });
+    const id = req.params.productId;
+    Product.remove({_id: id})
+        .exec()
+        .then(result =>{
+            res.status(200).json({
+                message:"Deleted product",
+                id: id
+            });
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            });
+            console.log(err);
+        });
 });
 
 
--- app.js
+++ app.js
@@ -1,11 +1,35 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const ProductRoutes = require('./api/routes/products');
 const OrderRoutes = require('./api/routes/orders')
 
+mongoose.connect("mongodb+srv://node-rest-api:"
+    +process.env.MONGO_ATLAS_PW+
+    "@node-rest-shop.lhegm.mongodb.net/node-test?retryWrites=true&w=majority",
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+
 app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+//CORS setting
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin','*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Origin');
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'POST, PUT, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
 
 //Routes handling requests
 app.use('/products', ProductRoutes);
